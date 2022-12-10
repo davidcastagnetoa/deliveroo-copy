3256c5ffcf17da21fd289742cfe0c5a8219af3b8
David Castagneto A <davidcastagnetoa@gmail.com>
2022-12-10 17:32:27 +0100

    some style problem with taiolwind

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,6 +2,12 @@ import { View, Text, StyleSheet, StatusBar, Image } from "react-native";
 import React, { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/core";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import {
+  ChevronDownIcon,
+  SearchIcon,
+  AdjustmentsHorizontalIcon,
+} from "react-native-heroicons/outline";
+import { UserIcon } from "react-native-heroicons/solid";
 
 const HomeScreen = () => {
   const navigation = useNavigation();
@@ -13,18 +19,27 @@ const HomeScreen = () => {
   }, []);
 
   return (
-    <SafeAreaProvider style={styles.container}>
+    <SafeAreaProvider classname="bg-blue pt-5" style={styles.container}>
       <Text className="text-red-500">
         {/* Header */}
-        <View className="flex-row pb-3 items-center mx-4 space-x-2">
+        <View className="flex flex-row pb-3 items-center mx-4 space-x-2 bg-red">
+        {/* Logo */}
           <Image
             source={require("../assets/sushiLogo.png")}
             className="h-12 w-12 bg-slate-100 p-4 rounded-full"
           />
-          <View>
-            <Text className="font-bold text-slate-400 text-xs">Entregar Ahora</Text>
-            <Text className="font bold text-xl">Ubicacion Actual</Text>
+          {/* Text */}
+          <View className="flex flex-1">
+            <Text className="font-bold text-slate-400 text-xs">
+              Entregar Ahora
+            </Text>
+            <Text className="font-bold text-xl">
+              Ubicacion Actual
+              <ChevronDownIcon size={20} color="#FF0000" />
+            </Text>
           </View>
+          {/* User Icon */}
+          <UserIcon size={35} color="#FF0000"/>
         </View>
       </Text>
     </SafeAreaProvider>
@@ -33,10 +48,9 @@ const HomeScreen = () => {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     marginTop: StatusBar.currentHeight + 10,
-    marginLeft: 10,
-    height: 34,
+    marginLeft: 8,
+    marginRight: 8,
   },
 });
 
